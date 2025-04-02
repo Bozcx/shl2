9a5de147aa38393b7ac880d821fc3d2d2fa785d4
Bozy <smartypants245@gmail.com>
2025-04-02 17:07:53 -0400

    Push

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Sbox Half Life 2 Mounting
 
 This repo contains a example for mounting Half Life 2 to the Sbox Engine, at the moment this does nothing more then simply telling the engine that hl2 is indeed installed on your pc.
+This is nothing more then a simple project I am doing for fun.
 
 # How to Use
 1. Build the Solution file
@@ -8,5 +9,3 @@ This repo contains a example for mounting Half Life 2 to the Sbox Engine, at the
 3. Create a new folder that has the same name as the dll `sbox/mount/shl2`
 4. Place the dll
 5. Mounted
-
-
---
+++ src/HL2Mount.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox.Mounting;
+
+namespace src;
+
+public class HL2Mount : BaseGameMount
+{
+    private string? Path { get; set; }
+
+    public override string Ident { get; } = "hl2";
+    public override string Title { get; } = "Half-Life 2";
+
+    protected override void Initialize(InitializeContext context)
+    {
+        if (!context.IsAppInstalled(220L)) return;
+        
+        Path = context.GetAppDirectory(220L) + "/hl2.exe";
+        
+        this.IsInstalled = File.Exists(this.Path);
+    }
+
+    protected override Task Mount(MountContext context)
+    {
+        if (!File.Exists(this.Path))
+        {
+            context.AddError(this.Path + " is not installed");
+            return Task.CompletedTask;
+        }
+        this.IsMounted = true;
+        return Task.CompletedTask;
+    }
+}
